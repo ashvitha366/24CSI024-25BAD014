bf47a8467d9942ad3480dd1ec052a61137506c24
ashvitha366 <ashvitha366@gmail.com>
2026-07-24 22:44:19 +0530

    Update Sliding Window Maximum Peak.c

--- Sliding Window Maximum Peak.c
+++ Sliding Window Maximum Peak.c
@@ -1,28 +1,31 @@
+
 #include <stdio.h>
-
 int main() {
-    int N, K;
-    scanf("%d %d", &N, &K);
-
-    int arr[N], deque[N];
-    for (int i = 0; i < N; i++)
-        scanf("%d", &arr[i]);
-
-    int front = 0, rear = -1;
-
+    int N, P;
+    printf("Enter the number of tasks: ");
+    scanf("%d", &N);
+    printf("Enter the maximum allowed energy consumption: ");
+    scanf("%d", &P);
+    int arr[N];
+    printf("Enter the energy costs of %d tasks: ", N);
     for (int i = 0; i < N; i++) {
-
-        while (front <= rear && deque[front] <= i - K)
-            front++;
-
-        while (front <= rear && arr[deque[rear]] <= arr[i])
-            rear--;
-
-        deque[++rear] = i;
-
-        if (i >= K - 1)
-            printf("%d ", arr[deque[front]]);
+        scanf("%d", &arr[i]);
     }
-
+    int start = 0;
+    int current_sum = 0;
+    int maxLength = 0;
+    for (int end = 0; end < N; end++) {
+        current_sum = current_sum + arr[end];
+        while (current_sum > P && start <= end) {
+            current_sum = current_sum - arr[start];
+            start++;
+        }
+        int length = end - start + 1;
+        if (length > maxLength) {
+            maxLength = length;
+        }
+    }
+    printf("Max Length: %d\n", maxLength);
     return 0;
 }
+
